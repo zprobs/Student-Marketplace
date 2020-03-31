1c95ebe91ef59e61a4022d662d6a3ddc30607077
stevsh <steven.shao@mail.mcgill.ca>
2020-03-30 23:52:04 -0400

    added 3 test cases for accounts

--- Backend/accounts/tests.py
+++ Backend/accounts/tests.py
@@ -1,3 +1,89 @@
-from django.test import TestCase
+from unittest.mock import Mock
 
-# Create your tests here.
+from django.contrib.auth.hashers import make_password
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from accounts.models import User
+from accounts.serializers import UserSerializer
+from accounts.views import RegisterView
+
+ID = 1
+EMAIL = 'test_email@test.com'
+PASSWORD = 'test_password'
+FIRST_NAME = 'first name'
+LAST_NAME = 'last name'
+SCHOOL = 'test school'
+ADDRESS = 'test address'
+PHONE = '1234567890'
+
+
+class ModelsTestCases(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, SCHOOL, ADDRESS, PHONE)
+
+    def test_create_user_create_expected_user(self):
+        actual = User.objects.get(email=EMAIL)
+        self.assertEqual(actual.email, EMAIL)
+        self.assertNotEqual(actual.password, PASSWORD)
+        self.assertEqual(actual.first_name, FIRST_NAME)
+        self.assertEqual(actual.last_name, LAST_NAME)
+        self.assertEqual(actual.school, SCHOOL)
+        self.assertEqual(actual.address, ADDRESS)
+        self.assertEqual(actual.phone, PHONE)
+        self.assertTrue(actual.is_active)
+        self.assertFalse(actual.is_staff)
+        self.assertFalse(actual.is_superuser)
+
+    def tearDown(self):
+        self.user.delete()
+
+
+class SerializersTestCases(TestCase):
+    def setUp(self):
+        self.expected_data = {
+            'id': ID,
+            'email': EMAIL,
+            'first_name': FIRST_NAME,
+            'last_name': LAST_NAME,
+            'school': SCHOOL,
+            'address': ADDRESS,
+            'phone': PHONE
+        }
+
+        self.user = User.objects.create_user(EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, SCHOOL, ADDRESS, PHONE)
+        self.serializer = UserSerializer(instance=self.user)
+
+    def test_user_serializer_contains_expected_fields(self):
+        actual_data = self.serializer.data
+
+        self.assertEqual(actual_data['id'], self.expected_data['id'])
+        self.assertEqual(actual_data['email'], self.expected_data['email'])
+        self.assertEqual(actual_data['first_name'], self.expected_data['first_name'])
+        self.assertEqual(actual_data['last_name'], self.expected_data['last_name'])
+        self.assertEqual(actual_data['school'], self.expected_data['school'])
+        self.assertEqual(actual_data['address'], self.expected_data['address'])
+        self.assertEqual(actual_data['phone'], self.expected_data['phone'])
+
+    def tearDown(self):
+        self.user.delete();
+
+
+class ViewsTestCases(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_register_success(self):
+        request = {
+            'email': EMAIL,
+            'password': PASSWORD,
+            'first_name': FIRST_NAME,
+            'last_name': LAST_NAME,
+            'school': SCHOOL,
+            'address': ADDRESS,
+            'phone': PHONE
+        }
+
+        response = self.client.post(reverse('register'), request)
+
+        self.assertEqual(response.status_code, 200)
--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -7,7 +7,7 @@ from .views import UserView, RegisterView, LoginView
 urlpatterns = [
     path('', include('knox.urls')),
     path('user', UserView.as_view()),
-    path('register', RegisterView.as_view()),
+    path('register', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view()),
     path('logout', LogoutView.as_view(), name='knox_logout')
 ]
