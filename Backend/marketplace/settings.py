12bac43fdf8e43fddbdc9e53897272b50f8bbbef
stevsh <steven.shao@mail.mcgill.ca>
2020-04-07 09:13:30 -0400

    added cors headers for front end

--- Backend/marketplace/settings.py
+++ Backend/marketplace/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'knox',
+    'corsheaders',
     'accounts',
     'listings',
     'transactions',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -148,3 +150,6 @@ MAILGUN_SERVER_NAME = 'sandbox4beadcc7211347e3ba5dec8dfedc41d9.mailgun.org'
 # image upload config
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# cors
+CORS_ORIGIN_ALLOW_ALL = True
