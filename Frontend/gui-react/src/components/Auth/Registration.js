27987cfd203c1d6e4eb16bd1757372d994970cef
zprobs <probst.zap@gmail.com>
2020-04-06 23:49:31 -0400

    xml request sign up

--- Frontend/gui-react/src/components/Auth/Registration.js
+++ Frontend/gui-react/src/components/Auth/Registration.js
@@ -11,10 +11,9 @@ class Registration extends Component {
             password:"",
             first_name:"",
             last_name:"",
+            phone_number:"",
             school:"",
             address:"",
-            phone_number:"",
-            registrationErrors:""
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -29,12 +28,15 @@ class Registration extends Component {
 
     handleSubmit(event) {
         console.log("form submitted");
+        console.log(JSON.stringify(this.state));
 
-        const {
-            email,
-            password,
-        } = this.state
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', () => {
+            console.log(xhr.responseText);
+        });
 
+        xhr.open('POST', 'http://127.0.0.1:8000/auth/register');
+        xhr.send(JSON.stringify(this.state));
         event.preventDefault();
     }
 
@@ -47,37 +49,37 @@ class Registration extends Component {
 
                     <div className="form-group">
                         <label>First name</label>
-                        <input type="text" className="form-control" placeholder="First name" value={this.state.first_name} onChange={this.handleChange} required  />
+                        <input type="text" name="first_name" className="form-control" placeholder="First name" value={this.state.first_name} onChange={this.handleChange} required  />
                     </div>
 
                     <div className="form-group">
                         <label>Last name</label>
-                        <input type="text" className="form-control" placeholder="Last name" value={this.state.last_name} onChange={this.handleChange} required  />
+                        <input type="text" name="last_name" className="form-control" placeholder="Last name" value={this.state.last_name} onChange={this.handleChange} required  />
                     </div>
 
                     <div className="form-group">
                         <label>School</label>
-                        <input type="text" className="form-control" placeholder="School" value={this.state.school} onChange={this.handleChange} required  />
+                        <input type="text" name="school" className="form-control" placeholder="School" value={this.state.school} onChange={this.handleChange} required  />
                     </div>
 
                     <div className="form-group">
                         <label>Address</label>
-                        <input type="text" className="form-control" placeholder="Address" value={this.state.address} onChange={this.handleChange} required  />
+                        <input type="text"name="address"  className="form-control" placeholder="Address" value={this.state.address} onChange={this.handleChange} required  />
                     </div>
 
                     <div className="form-group">
                         <label>Phone Number</label>
-                        <input type="tel" className="form-control" placeholder="Phone Number" value={this.state.phone_number} onChange={this.handleChange} required  />
+                        <input type="tel" name="phone_number" className="form-control" placeholder="Phone Number" value={this.state.phone_number} onChange={this.handleChange} required  />
                     </div>
 
                     <div className="form-group">
                         <label>Email address</label>
-                        <input type="email" className="form-control" placeholder="Enter email" value={this.state.email} onChange={this.handleChange} required  />
+                        <input type="email" name="email" className="form-control" placeholder="Enter email" value={this.state.email} onChange={this.handleChange} required  />
                     </div>
 
                     <div className="form-group">
                         <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Enter password" value={this.state.password} onChange={this.handleChange} required  />
+                        <input type="password" name="password" className="form-control" placeholder="Enter password" value={this.state.password} onChange={this.handleChange} required  />
                     </div>
 
                     <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
