0a767a14a6e4f114267a1f188f884a5811fb31d5
stevsh <steven.shao@mail.mcgill.ca>
2020-04-15 11:44:55 -0400

    restricted cors to localhost:3000 and :8000

--- Backend/marketplace/settings.py
+++ Backend/marketplace/settings.py
@@ -28,11 +28,13 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # cors
-CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_WHITELIST = [
-#     'localhost:3000',
-#     '127.0.0.1:3000'
-# ]
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+)
 
 # Application definition
 
